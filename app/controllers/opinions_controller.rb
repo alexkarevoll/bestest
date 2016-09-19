eeea0f37ad232b2e6256545e533110396e9ef126
Alex Karevoll <alex.karevoll@gmail.com>
2016-09-19 14:30:23 -0700

    added opinions view

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,33 @@
 class OpinionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def index
+    @opinions = Opinion.all
+  end
+
+  def show
+    @opinion = Opinion.find(params[:id])
+  end
+
+  def destroy
+    @opinion = Opinion.find(params[:id])
+    @opinion.destroy
+    redirect_to(opinions_path)
+  end
+
 end
