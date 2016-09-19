9a2e5be7d1e39e6d45dbb8ce4d64ea505e513ca5
Alex Karevoll <alex.karevoll@gmail.com>
2016-09-19 12:50:42 -0700

    fixed create user redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to_user_path(@user)
+    else
+      redirect_to_new_session_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to_root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to_new_session_path
+      redirect_to new_session_path
     end
   end
 
