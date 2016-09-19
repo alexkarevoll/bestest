dd8f1d0d367bfefd40078907f8b378947718f757
Alex Karevoll <alex.karevoll@gmail.com>
2016-09-19 13:00:08 -0700

    added log-in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to_user_path(@user)
+      redirect_to user_path(@user)
     else
-      redirect_to_new_session_path
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to_root_path
+    redirect_to root_path
   end
 end
